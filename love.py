b2b1cf4d9c2f446cc772b508a06fc9a922a2b71b
Yorks77 <77573410+Yorks77@users.noreply.github.com>
2021-12-17 23:40:54 +0200

    Upload file

---
+++ love.py
@@ -0,0 +1,96 @@
+# made by @DneZyeK
+
+
+from time import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.love(?: |$)(.*)')
+async def typewriter(typew):
+    await typew.edit("`ПОЖАЛУЙСТА ПРОЧТИ ДО КОНЦА`")
+    sleep(1)
+    await typew.edit("...")
+    number = 1
+    await typew.edit(str(number) + "♥️   Я")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   хочу")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   тебе")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   сказать")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   что")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   ты")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   самая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   лучшая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   ты")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   самая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   милая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   добрая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   хорошая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   красивая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   Улыбнись")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   ведь")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   тебе идет")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   и")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   кто-бы")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   чтобы")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   не говорил")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   знай")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   ты самая")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   прекрасная")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   в этом")
+    number = number + 1
+    sleep(1)
+    await typew.edit(str(number) + "♥️   мире♥️")
+    number = number + 1
+    sleep(2)
+    await typew.edit(str(number) + "♥️   С ЛЮБОВЬЮ")
+    number = number + 1
+    
+    sleep(1)
+    await typew.edit("`ТВОЙ ЛЮБИМЫЙ ЧЕЛОВЕК♥`")
